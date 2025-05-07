d089fa82c131bd7782aa7ef5ea67d520441506af
ASUS <ASUS@NIRMAL>
2025-05-08 00:52:50 +0530

    Dev-2 create obj and call bussiness method

--- Git_Project01/src/main/java/com/nirmal/service/App.java
+++ Git_Project01/src/main/java/com/nirmal/service/App.java
@@ -7,6 +7,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Task1 completed");
+        //create object for calculation 
+        Calculater cal=new Calculater();
+        //calling the bussines method
+        System.out.println(cal.doSum(12, 30));
         
     }
 }
