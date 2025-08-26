76a0596866d3baad625d1770dd60d65d233e8f0a
ASUS <ASUS@NIRMAL>
2025-08-27 00:31:39 +0530

    dev develop create acount operation

--- Git_Project01/src/main/java/com/nirmal/netbanking/NetBanking.java
+++ Git_Project01/src/main/java/com/nirmal/netbanking/NetBanking.java
@@ -1,5 +1,11 @@
 package com.nirmal.netbanking;
 
+import java.util.Random;
+
 public class NetBanking {
+	
+	public String createNewAccount(String adhar,String acountType,double amount) {
+		return "Acount is opened with "+acountType+" account number is :"+new Random().nextLong(90000000000L)+" , initial amount is :"+amount;
+	}
 
 }
