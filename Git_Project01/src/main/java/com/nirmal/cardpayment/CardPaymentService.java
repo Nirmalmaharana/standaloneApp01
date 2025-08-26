aa5669a6728184533972e3886570a93ec850ec51
ASUS <ASUS@NIRMAL>
2025-08-27 00:57:09 +0530

    developer  developed the cardpayment operation

--- Git_Project01/src/main/java/com/nirmal/cardpayment/CardPaymentService.java
+++ Git_Project01/src/main/java/com/nirmal/cardpayment/CardPaymentService.java
@@ -1,5 +1,9 @@
 package com.nirmal.cardpayment;
 
 public class CardPaymentService {
+	
+	public String cardPayment(Long cardnumber,double amount) {
+		return "INR:- "+amount+" paid by Card card number is "+cardnumber;
+	}
 
 }
