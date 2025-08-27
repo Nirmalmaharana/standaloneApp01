65313d80922bdaa343d666b5cbb9c23aa1edb7b4
ASUS <ASUS@NIRMAL>
2025-08-27 14:20:50 +0530

    tl  commit

--- Git_Project01/src/main/java/com/nirmal/cardpayment/CardPaymentService.java
+++ Git_Project01/src/main/java/com/nirmal/cardpayment/CardPaymentService.java
@@ -5,12 +5,11 @@ public class CardPaymentService {
 	public String cardPayment(Long cardnumber,double amount) {
 		return "INR:- "+amount+" paid by Card card number is "+cardnumber;
 	}
+	
+	public String withdrow(double amo,long aco) {
+		return amo+" amount withdrow from Account number :"+aco;
+	}
 
-	
-	
-	
-	
-	
 	public String closeAccount() {
 		return "Account is closed";
 	}
